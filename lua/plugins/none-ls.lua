d520a9619d87b42886afc3a62f4d7909b34589ff
TheCrow95T <calvinongjiajun@gmail,com>
2024-04-16 17:10:29 +0800

    fixed eslint, added prettier

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -9,8 +9,7 @@ return {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
-				-- null_ls.builtins.formatting.prettier,
-                -- require("none-ls.diagnostics.eslint_d"),
+				null_ls.builtins.formatting.prettier,
 			},
 		})
 	end,
