c6211b8d09a59f91770f171d8230957878669865
TheCrow95T <85043418+TheCrow95T@users.noreply.github.com>
2024-10-09 17:43:08 +0800

    added harpoon

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,55 +1,56 @@
 return {
-    {
-        "nvim-telescope/telescope.nvim",
-        event = "VeryLazy",
-        tag = "0.1.6",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "nvim-lua/popup.nvim",
-            "nvim-telescope/telescope-ui-select.nvim",
-            "nvim-telescope/telescope-project.nvim",
-            "nvim-telescope/telescope-file-browser.nvim",
-            "TheCrow95T/telescope-media-files.nvim",
-        },
-        config = function()
-            local builtin = require("telescope.builtin")
-
-            local actions = require("telescope.actions")
-            require("telescope").setup({
-                pickers = {
-                    buffers = {
-                        mappings = {
-                            i = {
-                                ["<C-w>"] = actions.delete_buffer + actions.move_to_top,
-                            },
-                        },
-                    },
-                },
-                extensions = {
-                    ["ui-select"] = {
-                        require("telescope.themes").get_dropdown({}),
-                    },
-                },
-            })
-
-            require("telescope").load_extension("ui-select")
-            require("telescope").load_extension("project")
-            require("telescope").load_extension("media_files")
-
-            -- keybindings
-            local keymap = vim.keymap.set
-
-            keymap("n", "<leader><leader>", builtin.find_files, { desc = "Find file from current directory" })
-            keymap("n", "<leader>/", builtin.live_grep, { desc = "Find text from current directory" })
-            keymap("n", "<leader>fh", builtin.help_tags, { desc = "Find help tags" })
-            keymap("n", "<leader>ft", ":TodoTelescope<CR>", { desc = "Find TODO from current directory" })
-            keymap("n", "<leader>fp", ":Telescope project<CR>", { desc = "Find Project" })
-            keymap("n", "<leader>fi", ":Telescope media_files<CR>", { desc = "Find media files" })
-            keymap("n", "<leader>fk", ":Telescope keymaps<CR>", { desc = "Find Keymapping" })
-            keymap("n", "<leader>fm", ":Telescope marks<CR>", { desc = "Find Mark" })
-            keymap("n", "<leader>fn", ":Telescope noice<CR>", { desc = "Find Noice message" })
-            keymap("n", "<leader>fb", ":Telescope buffers<CR>", { desc = "Find buffers" })
-            keymap("n", "<leader>fc", ":Telescope colorscheme<CR>", { desc = "Find buffers" })
-        end,
+  {
+    "nvim-telescope/telescope.nvim",
+    event = "VeryLazy",
+    tag = "0.1.6",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-lua/popup.nvim",
+      "nvim-telescope/telescope-ui-select.nvim",
+      "nvim-telescope/telescope-project.nvim",
+      "nvim-telescope/telescope-file-browser.nvim",
+      "TheCrow95T/telescope-media-files.nvim",
     },
+    config = function()
+      local builtin = require("telescope.builtin")
+
+      local actions = require("telescope.actions")
+      require("telescope").setup({
+        pickers = {
+          buffers = {
+            mappings = {
+              i = {
+                ["<C-w>"] = actions.delete_buffer + actions.move_to_top,
+              },
+            },
+          },
+        },
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
+          },
+        },
+      })
+
+      require("telescope").load_extension("ui-select")
+      require("telescope").load_extension("project")
+      require("telescope").load_extension("media_files")
+      require("telescope").load_extension("harpoon")
+
+      -- keybindings
+      local keymap = vim.keymap.set
+
+      keymap("n", "<leader><leader>", builtin.find_files, { desc = "Find file from current directory" })
+      keymap("n", "<leader>/", builtin.live_grep, { desc = "Find text from current directory" })
+      keymap("n", "<leader>fh", builtin.help_tags, { desc = "Find help tags" })
+      keymap("n", "<leader>ft", ":TodoTelescope<CR>", { desc = "Find TODO from current directory" })
+      keymap("n", "<leader>fp", ":Telescope project<CR>", { desc = "Find Project" })
+      keymap("n", "<leader>fi", ":Telescope media_files<CR>", { desc = "Find media files" })
+      keymap("n", "<leader>fk", ":Telescope keymaps<CR>", { desc = "Find Keymapping" })
+      keymap("n", "<leader>fm", ":Telescope harpoon marks<CR>", { desc = "Find Mark" })
+      keymap("n", "<leader>fn", ":Telescope noice<CR>", { desc = "Find Noice message" })
+      keymap("n", "<leader>fb", ":Telescope buffers<CR>", { desc = "Find buffers" })
+      keymap("n", "<leader>fc", ":Telescope colorscheme<CR>", { desc = "Find buffers" })
+    end,
+  },
 }
--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -1,122 +1,134 @@
 return {
-	{
-		"folke/todo-comments.nvim",
-		event = "VeryLazy",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		opts = {},
-	},
-	{
-		"windwp/nvim-ts-autotag",
-		event = "VeryLazy",
-		config = function()
-			require("nvim-ts-autotag").setup()
-		end,
-	},
-	{
-		"brenoprata10/nvim-highlight-colors",
-		event = "VeryLazy",
-		config = function()
-			require("nvim-highlight-colors").setup()
-		end,
-	},
-	{
-		"kdheepak/lazygit.nvim",
-		event = "VeryLazy",
-		cmd = {
-			"LazyGit",
-			"LazyGitConfig",
-			"LazyGitCurrentFile",
-			"LazyGitFilter",
-			"LazyGitFilterCurrentFile",
-		},
-	},
-	{
-		"kelly-lin/ranger.nvim",
-		event = "VeryLazy",
-		config = function()
-			require("ranger-nvim").setup({
-				replace_netrw = true,
-				ui = {
-					height = 0.9,
-					width = 0.9,
-					border = "single",
-				},
-			})
-		end,
-	},
-	{
-		"0x00-ketsu/maximizer.nvim",
-		event = "VeryLazy",
-		config = function()
-			require("maximizer").setup()
-		end,
-	},
-	{
-		"kevinhwang91/nvim-ufo",
-		event = "VeryLazy",
-		dependencies = "kevinhwang91/promise-async",
-		config = function()
-			vim.keymap.set("n", "zR", require("ufo").openAllFolds, { desc = "Open all folds" })
-			vim.keymap.set("n", "zM", require("ufo").closeAllFolds, { desc = "Close all folds" })
-			vim.keymap.set("n", "zK", function()
-				local winid = require("ufo").peekFoldedLinesUnderCursor()
-				if not winid then
-					vim.lsp.buf.hover()
-				end
-			end, { desc = "Peek Fold" })
+  {
+    "folke/todo-comments.nvim",
+    event = "VeryLazy",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {},
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
+  {
+    "brenoprata10/nvim-highlight-colors",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-highlight-colors").setup()
+    end,
+  },
+  {
+    "kdheepak/lazygit.nvim",
+    event = "VeryLazy",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+  },
+  {
+    "kelly-lin/ranger.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("ranger-nvim").setup({
+        replace_netrw = true,
+        ui = {
+          height = 0.9,
+          width = 0.9,
+          border = "single",
+        },
+      })
+    end,
+  },
+  {
+    "0x00-ketsu/maximizer.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("maximizer").setup()
+    end,
+  },
+  {
+    "ThePrimeagen/harpoon",
+    event = "VeryLazy",
+    config = function()
+      vim.keymap.set(
+        "n",
+        "<Space>m",
+        "<cmd>lua require('harpoon.mark').add_file()<CR>",
+        { desc = "Harpoon add file" }
+      )
+    end,
+  },
+  {
+    "kevinhwang91/nvim-ufo",
+    event = "VeryLazy",
+    dependencies = "kevinhwang91/promise-async",
+    config = function()
+      vim.keymap.set("n", "zR", require("ufo").openAllFolds, { desc = "Open all folds" })
+      vim.keymap.set("n", "zM", require("ufo").closeAllFolds, { desc = "Close all folds" })
+      vim.keymap.set("n", "zK", function()
+        local winid = require("ufo").peekFoldedLinesUnderCursor()
+        if not winid then
+          vim.lsp.buf.hover()
+        end
+      end, { desc = "Peek Fold" })
 
-			require("ufo").setup({
-				provider_selector = function(bufnr, filetype, buftype)
-					return { "lsp", "indent" }
-				end,
-			})
-		end,
-	},
-	{
-		"folke/flash.nvim",
-		event = "VeryLazy",
-		vscode = true,
-		keys = {
-			{
-				"s",
-				mode = { "n", "x", "o" },
-				function()
-					require("flash").jump()
-				end,
-				desc = "Flash",
-			},
-			{
-				"S",
-				mode = { "n", "o", "x" },
-				function()
-					require("flash").treesitter()
-				end,
-				desc = "Flash Treesitter",
-			},
-			{
-				"r",
-				mode = "o",
-				function()
-					require("flash").remote()
-				end,
-				desc = "Remote Flash",
-			},
-			{
-				"R",
-				mode = { "o", "x" },
-				function()
-					require("flash").treesitter_search()
-				end,
-				desc = "Treesitter Search",
-			},
-			{
-				"<c-s>",
-				mode = { "c" },
-				function()
-					require("flash").toggle()
-				end,
-				desc = "Toggle Flash Search",
-			},
-		},
-	},
+      require("ufo").setup({
+        provider_selector = function(bufnr, filetype, buftype)
+          return { "lsp", "indent" }
+        end,
+      })
+    end,
+  },
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    vscode = true,
+    keys = {
+      {
+        "s",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash").jump()
+        end,
+        desc = "Flash",
+      },
+      {
+        "S",
+        mode = { "n", "o", "x" },
+        function()
+          require("flash").treesitter()
+        end,
+        desc = "Flash Treesitter",
+      },
+      {
+        "r",
+        mode = "o",
+        function()
+          require("flash").remote()
+        end,
+        desc = "Remote Flash",
+      },
+      {
+        "R",
+        mode = { "o", "x" },
+        function()
+          require("flash").treesitter_search()
+        end,
+        desc = "Treesitter Search",
+      },
+      {
+        "<c-s>",
+        mode = { "c" },
+        function()
+          require("flash").toggle()
+        end,
+        desc = "Toggle Flash Search",
+      },
+    },
+  },
 }
