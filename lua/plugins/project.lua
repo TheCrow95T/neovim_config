ac02c16eb7245c813f3526e7f1d526b37ef2bd55
thecrow95t <85043418+TheCrow95T@users.noreply.github.com>
2025-03-03 22:37:52 +0800

    change project.nvim settings where it will automatically change root directory

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -1,6 +1,9 @@
 return {
     "ahmedkhalf/project.nvim",
     event = "VeryLazy",
+    opt = {
+        manual_mode = true,
+    },
     config = function()
         require("project_nvim").setup()
     end,
