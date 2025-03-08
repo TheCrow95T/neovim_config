ec479ed209ed4394b2758c8262fb5ac907176b3b
thecrow95t <85043418+TheCrow95T@users.noreply.github.com>
2025-03-09 03:58:58 +0800

    added keymap for zk

--- lua/plugins/notes.lua
+++ lua/plugins/notes.lua
@@ -2,6 +2,14 @@ return {
 	{
 		"zk-org/zk-nvim",
 		event = "VeryLazy",
+		keys = {
+			{ "<leader>nn", "<cmd>ZkNew { title = vim.fn.input('Title: '), dir = 'Ideas' }<cr>", desc = "Create new notes" },
+			{ "<leader>nj", "<cmd>ZkNew { group = 'daily', dir = 'Journal' }<cr>", desc = "Create new Journal" },
+			{ "<leader>nl", "<cmd>ZkNew { group = 'learn', dir = 'Learn' }<cr>", desc = "Create new Learning notes" },
+			{ "<leader>ns", "<cmd>ZkNew { group = 'standup', dir = 'StandUp' }<cr>", desc = "Create new Stand up" },
+      { "<leader>nf", "<cmd>ZkNotes<cr>", desc = "Search notes" },
+			{ "<leader>nt", "<cmd>ZkTags<cr>", desc = "Search notes by tag" },
+		},
 		config = function()
 			require("zk").setup({
 				picker = "telescope",
