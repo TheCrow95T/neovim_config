9784226be2659a2d34928307548520c279d5af6d
thecrow95t <85043418+TheCrow95T@users.noreply.github.com>
2025-03-09 12:39:43 +0800

    added title to "learn" notes

--- lua/plugins/notes.lua
+++ lua/plugins/notes.lua
@@ -3,14 +3,46 @@ return {
 		"zk-org/zk-nvim",
 		event = "VeryLazy",
 		keys = {
-			{ "<leader>nn", "<cmd>ZkNew { title = vim.fn.input('Title: '), dir = 'Ideas' }<cr>", desc = "Create new notes" },
-			{ "<leader>nj", "<cmd>ZkNew { group = 'daily', dir = 'Journal' }<cr>", desc = "Create new Journal" },
-			{ "<leader>nl", "<cmd>ZkNew { group = 'learn', dir = 'Learn' }<cr>", desc = "Create new Learning notes" },
-			{ "<leader>ns", "<cmd>ZkNew { group = 'standup', dir = 'StandUp' }<cr>", desc = "Create new Stand up" },
-      { "<leader>nf", "<cmd>ZkNotes<cr>", desc = "Search notes" },
-			{ "<leader>nt", "<cmd>ZkTags<cr>", desc = "Search notes by tag" },
-			{ "<leader>ng", "<cmd>ZkLinks<cr>", desc = "Search notes by links" },
-			{ "<leader>nG", "<cmd>ZkBacklinks<cr>", desc = "Search notes by back links" },
+			{
+				"<leader>nn",
+				"<cmd>ZkNew { title = vim.fn.input('Title: '), dir = 'Ideas' }<cr>",
+				desc = "Create new notes",
+			},
+			{
+				"<leader>nj",
+				"<cmd>ZkNew { group = 'daily', dir = 'Journal' }<cr>",
+				desc = "Create new Journal",
+			},
+			{
+				"<leader>nl",
+				"<cmd>ZkNew { title = vim.fn.input('Title: '), group = 'learn', dir = 'Learn' }<cr>",
+				desc = "Create new Learning notes",
+			},
+			{
+				"<leader>ns",
+				"<cmd>ZkNew { group = 'standup', dir = 'StandUp' }<cr>",
+				desc = "Create new Stand up",
+			},
+			{
+				"<leader>nf",
+				"<cmd>ZkNotes<cr>",
+				desc = "Search notes",
+			},
+			{
+				"<leader>nt",
+				"<cmd>ZkTags<cr>",
+				desc = "Search notes by tag",
+			},
+			{
+				"<leader>ng",
+				"<cmd>ZkLinks<cr>",
+				desc = "Search notes by links",
+			},
+			{
+				"<leader>nG",
+				"<cmd>ZkBacklinks<cr>",
+				desc = "Search notes by back links",
+			},
 		},
 		config = function()
 			require("zk").setup({
@@ -36,13 +68,13 @@ return {
 		---@type render.md.UserConfig
 		opts = {},
 	},
-  {
-    "AndrewRadev/switch.vim",
+	{
+		"AndrewRadev/switch.vim",
 		event = "VeryLazy",
 		keys = {
 			{ "<leader>s", "<cmd>Switch<cr>", desc = "Power of switch magic on code" },
 		},
-  },
+	},
 	{
 		"HakonHarnes/img-clip.nvim",
 		event = "VeryLazy",
