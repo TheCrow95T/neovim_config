1b05d741293269f04d1e33d7604363f560cb9f52
thecrow95t <85043418+TheCrow95T@users.noreply.github.com>
2025-04-09 01:10:31 +0800

    added easy dotnet

--- lua/plugins/language-extra.lua
+++ lua/plugins/language-extra.lua
@@ -3,14 +3,29 @@ return {
 		"mfussenegger/nvim-jdtls",
 		event = "VeryLazy",
 	},
-  {
-    "seblyng/roslyn.nvim",
-  event = "VeryLazy",
-    ft = "cs",
-    ---@module 'roslyn.config'
-    ---@type RoslynNvimConfig
-    opts = {
-        -- your configuration comes here; leave empty for default settings
-    }
-  }
+	{
+		"seblyng/roslyn.nvim",
+		event = "VeryLazy",
+		ft = "cs",
+		---@module 'roslyn.config'
+		---@type RoslynNvimConfig
+		opts = {
+			-- your configuration comes here; leave empty for default settings
+		},
+	},
+	{
+		"GustavEikaas/easy-dotnet.nvim",
+		event = "VeryLazy",
+		dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+		config = function()
+			require("easy-dotnet").setup({
+        picker = "snacks"
+      })
+
+      -- keybindings
+      local keymap = vim.keymap.set
+
+      keymap("n", "<leader>dc", "<cmd>Dotnet<CR>", { desc = "Dotnet command" })
+		end,
+	},
 }
