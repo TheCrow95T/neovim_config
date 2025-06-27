99e909a96fe2f13943ada87c65472109a0740ff9
thecrow95t <85043418+TheCrow95T@users.noreply.github.com>
2025-06-27 19:43:57 +0800

    fix some blink stuff for easydotnet

--- lua/plugins/autocompletion.lua
+++ lua/plugins/autocompletion.lua
@@ -1,66 +1,75 @@
 return {
-	{ -- Autocompletion
-		"saghen/blink.cmp",
-		event = "VeryLazy",
-		version = "1.*",
-		dependencies = {
-			-- Snippet Engine
-			{
-				"L3MON4D3/LuaSnip",
-				version = "2.*",
-				build = (function()
-					-- Build Step is needed for regex support in snippets.
-					-- This step is not supported in many windows environments.
-					-- Remove the below condition to re-enable on windows.
-					if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
-						return
-					end
-					return "make install_jsregexp"
-				end)(),
-				dependencies = {
-					-- `friendly-snippets` contains a variety of premade snippets.
-					--    See the README about individual language/framework/plugin snippets:
-					--    https://github.com/rafamadriz/friendly-snippets
-					{
-						"rafamadriz/friendly-snippets",
-						config = function()
-							require("luasnip.loaders.from_vscode").lazy_load()
-						end,
-					},
-				},
-				opts = {},
-			},
-			"folke/lazydev.nvim",
-		},
-		--- @module 'blink.cmp'
-		--- @type blink.cmp.Config
-		opts = {
-			keymap = {
-				preset = "enter",
-			},
-			appearance = {
-				nerd_font_variant = "mono",
-			},
-			completion = {
-				documentation = { auto_show = true, auto_show_delay_ms = 200 },
-				trigger = {
-					show_on_x_blocked_trigger_characters = { "'", '"', "(", "{" },
-				},
-			},
-			sources = {
-				default = { "lsp", "path", "snippets" },
-			},
-			snippets = { preset = "luasnip" },
-			fuzzy = {
-				implementation = "prefer_rust_with_warning",
-				sorts = {
-					"exact",
-					-- defaults
-					"score",
-					"sort_text",
-				},
-			},
-			signature = { enabled = false },
-		},
-	},
+  { -- Autocompletion
+    "saghen/blink.cmp",
+    event = "VeryLazy",
+    version = "1.*",
+    dependencies = {
+      -- Snippet Engine
+      {
+        "L3MON4D3/LuaSnip",
+        version = "2.*",
+        build = (function()
+          -- Build Step is needed for regex support in snippets.
+          -- This step is not supported in many windows environments.
+          -- Remove the below condition to re-enable on windows.
+          if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
+            return
+          end
+          return "make install_jsregexp"
+        end)(),
+        dependencies = {
+          -- `friendly-snippets` contains a variety of premade snippets.
+          --    See the README about individual language/framework/plugin snippets:
+          --    https://github.com/rafamadriz/friendly-snippets
+          {
+            "rafamadriz/friendly-snippets",
+            config = function()
+              require("luasnip.loaders.from_vscode").lazy_load()
+            end,
+          },
+        },
+        opts = {},
+      },
+      "folke/lazydev.nvim",
+    },
+    --- @module 'blink.cmp'
+    --- @type blink.cmp.Config
+    opts = {
+      keymap = {
+        preset = "enter",
+      },
+      appearance = {
+        nerd_font_variant = "mono",
+      },
+      completion = {
+        documentation = { auto_show = true, auto_show_delay_ms = 200 },
+        trigger = {
+          show_on_x_blocked_trigger_characters = { "'", '"', "(", "{" },
+        },
+      },
+      sources = {
+        default = { "lsp", "path", "snippets", "easy-dotnet" },
+        providers = {
+          ["easy-dotnet"] = {
+            name = "easy-dotnet",
+            enabled = true,
+            module = "easy-dotnet.completion.blink",
+            score_offset = 10000,
+            async = true,
+          },
+        },
+      },
+      snippets = { preset = "luasnip" },
+      fuzzy = {
+        implementation = "prefer_rust_with_warning",
+        sorts = {
+          "exact",
+          -- defaults
+          "score",
+          "sort_text",
+        },
+      },
+      signature = { enabled = false },
+    },
+  },
 }
